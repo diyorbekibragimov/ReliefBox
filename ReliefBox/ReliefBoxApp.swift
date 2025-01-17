f54ef7783d15aac561a9f2a7c9e793831403d5f8
Diyorbek Ibragimov <dierbekibragimov@gmail.com>
2025-01-17 17:58:02 +0300

    Initial Commit

---
+++ ReliefBox/ReliefBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReliefBoxApp.swift
+//  ReliefBox
+//
+//  Created by Diyorbek Ibragimov on 17/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct ReliefBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReliefBoxTests/ReliefBoxTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReliefBoxTests.swift
+//  ReliefBoxTests
+//
+//  Created by Diyorbek Ibragimov on 17/01/2025.
+//
+
+import Testing
+@testable import ReliefBox
+
+struct ReliefBoxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
